456348c172bbc7e8417452ebf3f1fed37ae0bd25
Gabi Udrescu <gabriel.udr@gmail.com>
2015-08-09 21:13:09 +0300

    adding functional testing for job create and preview form

--- src/GabiU/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/GabiU/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -11,6 +11,7 @@ namespace GabiU\JobeetBundle\Tests\Controller;
 use GabiU\JobeetBundle\Utils\Jobeet as Utils;
 use GabiU\JobeetBundle\Tests\FunctionalFixturesTestCase;
 use GabiU\JobeetBundle\Entity\Job;
+use Symfony\Component\DomCrawler\Crawler;
 
 class JobControllerTest extends FunctionalFixturesTestCase {
     public function testIndex()
@@ -109,5 +110,140 @@ class JobControllerTest extends FunctionalFixturesTestCase {
         $client->followRedirect();
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testJobForm()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request("GET", "/job/new");
+        $this->assertContains('GabiU\JobeetBundle\Controller\JobController::newAction', $client->getRequest()->attributes->get('_controller'));
+
+        $formData = array(
+            "gabiu_jobeetbundle_job[category]" => 1, //aka Design
+            "gabiu_jobeetbundle_job[type]" => "freelance",
+            "gabiu_jobeetbundle_job[company]" => "Sensio Labs",
+            "gabiu_jobeetbundle_job[logoFile]" => __DIR__.'/../../../../../web/bundles/gabiujobeet/images/sensio-labs.gif',
+            "gabiu_jobeetbundle_job[position]" => "Senior designer",
+            "gabiu_jobeetbundle_job[url]" => "https://sensiolabs.com/",
+            "gabiu_jobeetbundle_job[location]" => "Bucharest",
+            "gabiu_jobeetbundle_job[description]" => "Lorem ipsum description",
+            "gabiu_jobeetbundle_job[howToApply]" => "Send email to jobs@sensiolabs.com",
+            "gabiu_jobeetbundle_job[isPublic]" => true,
+            "gabiu_jobeetbundle_job[email]" => 'jobs@sensiolabs.com',
+        );
+
+        $form = $crawler->selectButton("Create")->form($formData);
+
+        $client->submit($form);
+        $this->assertContains('GabiU\JobeetBundle\Controller\JobController::createAction', $client->getRequest()->attributes->get('_controller'));
+
+
+        $client->followRedirect();
+        $this->assertContains('GabiU\JobeetBundle\Controller\JobController::previewAction', $client->getRequest()->attributes->get('_controller'));
+    }
+
+    /**
+     * there are three errors we're looking after:
+     * 1. description is not set
+     * 2. how to apply is not set
+     * 3. email is not valid
+     *
+     * isPublic is defaulting to true as per Doctrine Entity configuration and it will not show an error
+     */
+    public function testJobFormCreateShowsErrorIfNotFullyCompleted()
+    {
+        $formData = array(
+            "gabiu_jobeetbundle_job[category]" => 1, //aka Design
+            "gabiu_jobeetbundle_job[type]" => "freelance",
+            "gabiu_jobeetbundle_job[company]" => "Sensio Labs",
+            "gabiu_jobeetbundle_job[logoFile]" => __DIR__.'/../../../../../web/bundles/gabiujobeet/images/sensio-labs.gif',
+            "gabiu_jobeetbundle_job[position]" => "Senior designer",
+            "gabiu_jobeetbundle_job[url]" => "https://sensiolabs.com/",
+            "gabiu_jobeetbundle_job[location]" => "Bucharest",
+            "gabiu_jobeetbundle_job[email]" => 'not.an.email',
+        );
+        $client = $this->createJob($formData);
+
+        $crawler = $client->getCrawler();
+
+        /**
+         * get HTML errors as a plain message
+         */
+        $errors = implode("\n\n\n\"", $crawler->filter(".error_list")->each(function(Crawler $crawler){
+            return $crawler->parents()->html();
+        }));
+        $this->assertEquals(3, $crawler->filter(".error_list")->count(), $errors);
+
+        $this->assertEquals(1, $crawler->filter("#gabiu_jobeetbundle_job_howToApply")->siblings()->first()->filter(".error_list")->count());
+        $this->assertEquals(1, $crawler->filter("#gabiu_jobeetbundle_job_description")->siblings()->first()->filter(".error_list")->count());
+        $this->assertEquals(0, $crawler->filter("#gabiu_jobeetbundle_job_isPublic")->siblings()->first()->filter(".error_list")->count());
+
+        $this->assertEquals(1, $crawler->filter("#gabiu_jobeetbundle_job_email")->siblings()->first()->filter(".error_list")->count());
+    }
+
+    private function createJob($values = array()){
+        $client = static::createClient();
+
+        $crawler = $client->request("GET", "/job/new");
+
+        $formData = array(
+            "gabiu_jobeetbundle_job[category]" => 1, //aka Design
+            "gabiu_jobeetbundle_job[type]" => "freelance",
+            "gabiu_jobeetbundle_job[company]" => "Sensio Labs",
+            "gabiu_jobeetbundle_job[logoFile]" => __DIR__.'/../../../../../web/bundles/gabiujobeet/images/sensio-labs.gif',
+            "gabiu_jobeetbundle_job[position]" => "Senior designer",
+            "gabiu_jobeetbundle_job[url]" => "https://sensiolabs.com/",
+            "gabiu_jobeetbundle_job[location]" => "Bucharest",
+//            "gabiu_jobeetbundle_job[description]" => "Lorem ipsum description",
+//            "gabiu_jobeetbundle_job[howToApply]" => "Send email to jobs@sensiolabs.com",
+//            "gabiu_jobeetbundle_job[isPublic]" => true,
+            "gabiu_jobeetbundle_job[email]" => 'test@test.com',
+        );
+
+        $form = $crawler->selectButton("Create")->form(array_merge($formData, $values));
+        $client->submit($form);
+
+        return $client;
+    }
+
+    public function testPublishJob()
+    {
+        $formData = array_merge($this->formData, array("gabiu_jobeetbundle_job[position]" => "F001"));
+        $client = $this->createJob($formData);
+        $client->followRedirect();
+
+
+        $crawler = $client->getCrawler();
+        $form = $crawler->selectButton("Publish")->form();
+
+        $client->submit($form);
+
+        $query = $this->em->createQuery("select count(j.id) from GabiUJobeetBundle:Job j where j.position = :position and j.isActivated = 1");
+        $query->setParameter("position", "F001");
+        $this->assertEquals(1, $query->getSingleScalarResult());
+    }
+
+    private $formData = array(
+        "gabiu_jobeetbundle_job[description]" => "Lorem ipsum description",
+        "gabiu_jobeetbundle_job[howToApply]" => "Send email to jobs@sensiolabs.com",
+        "gabiu_jobeetbundle_job[isPublic]" => true,
+    );
+
+    public function testDeleteJob()
+    {
+        $formData = array_merge($this->formData, array("gabiu_jobeetbundle_job[position]" => "F002"));
+        $client = $this->createJob($formData);
+        $client->followRedirect();
+
+
+        $crawler = $client->getCrawler();
+        $form = $crawler->selectButton("Delete")->form();
+
+        $client->submit($form);
+
+        $query = $this->em->createQuery("select count(j.id) from GabiUJobeetBundle:Job j where j.position = :position");
+        $query->setParameter("position", "F002");
+        $this->assertEquals(0, $query->getSingleScalarResult());
     }
 }
