32145949fd391d29d09593bb47dd51cf5d19aa39
Gabi Udrescu <gabriel.udr@gmail.com>
2015-08-09 16:51:31 +0300

    admin with security

--- src/GabiU/JobeetBundle/Controller/AdminController.php
+++ src/GabiU/JobeetBundle/Controller/AdminController.php
@@ -27,4 +27,31 @@ class AdminController extends BaseAdminController
             "entity" => $this->request->query->get('entity')
         ));
     }
+
+    public function loginAction()
+    {
+
+        $authUtils = $this->get("security.authentication_utils");
+
+        if ($error = $authUtils->getLastAuthenticationError())
+        {
+            $error = $error->getMessage();
+        }
+
+        $lastUsername = $authUtils->getLastUsername();
+
+        $form = $this->createFormBuilder()
+            ->add("username","text", array("label" => false, "attr" => array("placeholder" => "Username")))
+            ->add("password", "password", array("label" => false, "attr" => array("placeholder" => "Password")))
+            ->add("submit", "submit", array("label" => "Login"));
+
+        $form->setAction($this->generateUrl("login_check"))
+            ->setMethod("POST");
+
+        return $this->render("@GabiUJobeet/Admin/login.html.twig", array(
+            "form" => $form->getForm()->createView(),
+            "error" => $error,
+            "last_username" => $lastUsername
+        ));
+    }
 }
