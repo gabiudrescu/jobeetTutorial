ebaa4d63c6c5683d395c450c6b7259aab8a52138
Gabi Udrescu <gabriel.udr@gmail.com>
2015-08-31 21:17:33 +0300

    capifony config done + generator bundle moved to prod

---
+++ app/config/deploy.rb
@@ -0,0 +1,33 @@
+set :application, "jobeetTutorial"
+set :domain,      "jobeet.gabiudrescu.com"
+set :deploy_to,   "/#{domain}"
+set :app_path,    "app"
+
+set :repository,  "https://github.com/gabiudrescu/jobeetTutorial.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set   :use_sudo,      false
+set   :keep_releases, 3
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
+
+after "deploy", "deploy:cleanup"
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+
+set :use_composer, true
+set :update_vendors, true
+
+set :user, 'gabiudr'
+
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub), port: '22123' }
